a198b907c1bcd7c046bca974da724a6852f81e17
Abhipsa04 <64295503+Abhipsa04@users.noreply.github.com>
2021-11-29 10:25:26 +0530

    Create ASCIIofChar.java

---
+++ ASCIIofChar.java
@@ -0,0 +1,9 @@
+// "static void main" must be defined in a public class.
+//ASCII vaue of a character/
+public class Main {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    char c = sc.next().charAt(0);
+    System.out.println((int)c);
+    }
+}
