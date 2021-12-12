f386977bebef2a71fed049e7a0caa3522d38399c
Abhipsa04 <64295503+Abhipsa04@users.noreply.github.com>
2021-12-12 11:22:29 +0530

    Update and rename noofDistinctelementsinarr.java to Distinctelementsinarr.java

--- Arraysbasic/Distinctelementsinarr.java
+++ Arraysbasic/Distinctelementsinarr.java
@@ -9,20 +9,23 @@ public class Main {
         {
             arr[i]=sc.nextInt();
         }
-       System.out.println("no of Distinct elements "+ Distinct_count(arr,size)); 
+        Distinct_count(arr,size); 
         
     }
-    public static int Distinct_count(int[] arr , int n)
+    public static void Distinct_count(int[] arr , int n)
     {
         Arrays.sort(arr);
         int dis = 0;
+        System.out.println("Elements are : ");
         for(int i=0; i<n;i++){
             while(i<n-1 && arr[i]==arr[i+1]){
                 i++;
             }
             dis++;
+            System.out.println(arr[i]);
         }
-        return dis;
+        System.out.println("No of Distinct elements : " +dis);
+        
     }
     
 }
