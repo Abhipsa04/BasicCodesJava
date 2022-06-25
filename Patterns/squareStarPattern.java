6e21edfc6134fbb8eeecd2ac64d6ff005a2f9bd8
Abhipsa04 <64295503+Abhipsa04@users.noreply.github.com>
2022-06-25 20:05:38 +0530

    Square Star Pattern

--- Patterns/squareStarPattern.java
+++ Patterns/squareStarPattern.java
@@ -2,7 +2,7 @@ public class Main
 {
 	public static void main(String[] args) {
 		for(int i =0 ; i< 4; i++){ //outer for is for rows
-		    for(int j=0 ; j<4; j++){// inner for is for columns
+		    for(int j=0 ; j<4; j++){ // inner for is for columns
 		        System.out.print("*");
 		    }
 		    System.out.println();// for going to next line
