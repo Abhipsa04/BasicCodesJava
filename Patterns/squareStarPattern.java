930bf68b5f0f0db341624ce81d78428e02814ba9
Abhipsa04 <64295503+Abhipsa04@users.noreply.github.com>
2022-06-25 19:34:15 +0530

    Add files via upload

---
+++ Patterns/squareStarPattern.java
@@ -0,0 +1,11 @@
+public class Main
+{
+	public static void main(String[] args) {
+		for(int i =0 ; i< 4; i++){ //outer for is for rows
+		    for(int j=0 ; j<4; j++){// inner for is for columns
+		        System.out.print("*");
+		    }
+		    System.out.println();// for going to next line
+		}
+	}
+}
