250497aac7f245b367714ff62bac9a01c42550a0
Abhipsa04 <64295503+Abhipsa04@users.noreply.github.com>
2022-07-07 10:56:47 +0530

    Fibonacci using recursion 2 approaches

--- Basic Codes Inter/fiboRecur.java
+++ Basic Codes Inter/fiboRecur.java
@@ -19,3 +19,33 @@ public class Main
   printFibonacci(n-2);//n-2 because 2 numbers are already printed   
  } 
 }
+/* Another approach 
+class GFG {
+  
+    // Function to print the fibonacci series
+    static int fib(int n)
+    {
+        // Base Case
+        if (n <= 1)
+            return n;
+  
+        // Recursive call
+        return fib(n - 1)
+            + fib(n - 2);
+    }
+  
+    // Driver Code
+    public static void
+    main(String args[])
+    {
+        // Given Number N
+        int N = 10;
+  
+        // Print the first N numbers
+        for (int i = 0; i < N; i++) {
+  
+            System.out.print(fib(i) + " ");
+        }
+    }
+}
+*/
